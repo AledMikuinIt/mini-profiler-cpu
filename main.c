b4855b0798cebb328f9e57cf168109426ae0905b
XD <clement.muzard@etu.umontpellier.fr>
2025-12-03 11:53:35 +0100

    feat: double benchmark test

--- main.c
+++ main.c
@@ -1,12 +1,14 @@
 // main.c
+#include <stdint.h>
 #include <stdio.h>
 #include "profiler.h"
 
 void test_fn() {
-    for (volatile int i = 0; i < 1000; i++);
+    for (volatile int i = 0; i < 1000; i++);    // volatile here to sya to the compilator to not rearange & execute in this order
 }
 
 int main() {
     uint64_t cycles = measure_once(test_fn);
-    printf("Measure : %llu cycles\n", (unsigned long long)cycles);
+    uint64_t cycles_n = measure_n(test_fn, 20);
+    printf("Measure Once: %llu cycles\nMeasure N: %llu\n", (unsigned long long)cycles, (unsigned long long)cycles_n);
 }
