9c2864402193f4f98e79a16cfe1bdcab211496fd
XD <clement.muzard@etu.umontpellier.fr>
2025-12-03 10:12:09 +0100

    chore: add comments in english

--- profiler.c
+++ profiler.c
@@ -1,42 +1,42 @@
 #include "profiler.h"
 
-// Début de la mesure avec lfence & rdtsc
+// Start the TSC measurement w/ lfence & rdtsc & ASM
 
 uint64_t start_tsc() {
     unsigned lo, hi;
     __asm__ volatile (
-        "lfence\n\t"    // stop charger/exec spéculative via lfence
-        "rdtsc\n\t"     // lit ke tsc 
-        : "=a"(lo), "=d"(hi)
+        "lfence\n\t"    // stop the speculative with lfence
+        "rdtsc\n\t"     // read the TSC
+        : "=a"(lo), "=d"(hi)    // a takes the 32 low bits from EAX and d takes the 32 high bits from EDX
         :
-        : "memory"
+        : "memory"          // Clobber that says that its memory
     );
 
-    return ((uint64_t)hi << 32) | lo;
+    return ((uint64_t)hi << 32) | lo;   // return the 64 bits uint , shift 32 bits on the left for the hi and then OR the low bitss
 
 }
 
-// Fin de la mesure aved rdtsc & lfence
+// End the TSC measurement w/ rdtscp & lfence & ASM
 
 uint64_t stop_tsc() {
     unsigned lo, hi;
     __asm__ volatile (
-        "rdtscp\n\t"    // lit le TSC sérialisé
-        "lfence\n\t"    // lfence pour empecher de re ordonnement
-        : "=a"(lo), "=d"(hi)
+        "rdtscp\n\t"    // read the TSC serialized
+        "lfence\n\t"    // lfence to stop the re arrangement
+        : "=a"(lo), "=d"(hi)    // a takes 32 bits low etc
         : 
-        : "rcx", "memory"
+        : "rcx", "memory"   // Clobber that says that memory goes in the rcx so do not modify (to the compilator)
     );
 
-    return ((uint64_t)hi << 32) | lo;
+    return ((uint64_t)hi << 32) | lo;   // return a 64 unit bits with the 32 high | 32 low 
 
 }
 
-// Mesurer une exec d'une fonction fn
+// Measure the function time
 
 uint64_t measure_once(becnh_fn fn) {
     uint64_t begin = start_tsc();
-    fn();       // ce qu'on mesure
+    fn();       // measure the function
     uint64_t end = stop_tsc();
     return end - begin;
 }
