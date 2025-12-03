3f9e7af36069736e7352f3af4fb5f8bdfeb8028c
XD <clement.muzard@etu.umontpellier.fr>
2025-12-03 11:54:27 +0100

    feat: measure N time function for benchmark w/ warmup & return median

--- profiler.c
+++ profiler.c
@@ -1,4 +1,6 @@
 #include "profiler.h"
+#include <stdint.h>
+#include <stdlib.h>   // for qsort
 
 // Start the TSC measurement w/ lfence & rdtsc & ASM
 
@@ -25,18 +27,44 @@ uint64_t stop_tsc() {
         "lfence\n\t"    // lfence to stop the re arrangement
         : "=a"(lo), "=d"(hi)    // a takes 32 bits low etc
         : 
-        : "rcx", "memory"   // Clobber that says that memory goes in the rcx so do not modify (to the compilator)
+        : "rcx", "memory"   // Clobber that says that memory goes in the rcx so do not modify (to the compiler)
     );
 
     return ((uint64_t)hi << 32) | lo;   // return a 64 unit bits with the 32 high | 32 low 
 
 }
 
-// Measure the function time
+// Measure the function time once
 
-uint64_t measure_once(becnh_fn fn) {
+uint64_t measure_once(bench_fn fn) {
     uint64_t begin = start_tsc();
     fn();       // measure the function
     uint64_t end = stop_tsc();
     return end - begin;
+}
+
+
+// Compare function for uint64 for qsort
+int cmp_uint64(const void *a, const void *b) {
+    uint64_t ua = *(const uint64_t*)a;  // cast to a const ptr for test all As
+    uint64_t ub = *(const uint64_t*)b;  // cast to a const ptr for test all Bs
+    if (ua < ub) return -1; // a is less than b
+    if (ua > ub) return 1;  // a is more than b
+    return 0;
+}
+
+
+// Measure the function N times w/ warmup
+
+uint64_t measure_n(bench_fn fn, int n) {
+
+    uint64_t med[n];    // alloc a n sized median array
+
+    fn();               // warmup the function for the CPU cache, volatile in fn so it doesnt be rearranged
+    for(int i = 0; i<n; i++) {  // test N numbers of times
+        med[i] = measure_once(fn);
+    }
+    qsort(med, n, sizeof(uint64_t), cmp_uint64);    // sort the results to get the median
+
+    return med[n/2];    // return the median
 }
