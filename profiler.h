f48e88a7aca55d1583d9bd3b092d2f1a60a716da
XD <clement.muzard@etu.umontpellier.fr>
2025-12-03 11:54:05 +0100

    feat: measure N time function for benchmark w/ warmup

--- profiler.h
+++ profiler.h
@@ -3,10 +3,11 @@
 
 #include <stdint.h>
 
-typedef void (*becnh_fn)(void) ;
+typedef void (*bench_fn)(void) ;
 
 uint64_t start_tsc();
 uint64_t stop_tsc();
-uint64_t measure_once(becnh_fn fn);
+uint64_t measure_once(bench_fn fn);
+uint64_t measure_n(bench_fn fn, int n);
 
 #endif
